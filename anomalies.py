eef3d5f1a18b7de190bb3b7e0d69200757fb7ab2
alsmg98 <70724837+alsmg98@users.noreply.github.com>
2020-09-17 11:22:47 -0400

    Update anomalies.py

--- anomalies.py
+++ anomalies.py
@@ -10,40 +10,24 @@ import numpy as np
 
 from sp_iotsim.fileio import load_data
 
+std= statistic.pstdev(temp)
+print("Standard deviation temperature {:.5}" .format(str(std)))
 
-i = (temp > 19) & (temp < 25)
-filtered_temp = temp[i]
+temp_g = []
+temp_b = []
 
-temp.size
+for check in temp:
+    if((check-statistic.mean(temp))>=std*3):
+        tem_b.append(check)
+    else:
+        temp_g.append(check)
+print("Bad points: ", end="")
+for i in tem_b:
+    print("{:.5}" .format(float(i)), end="")
 
+print("The standard deviation without bad points: {:.5f}" .format(statistics.pstdev(temp_g)))
 
+print("{}".format(len(temp_b)))
+print("{}".format(len(temp_g)))
+print("{}".format(len(temp_b)+len(temp_g)))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    p = argparse.ArgumentParser(description="load and analyse IoT JSON data")
-    p.add_argument("file", help="path to JSON data file")
-    p.add_argument("room", help="room to plot")
-    P = p.parse_args()
-
-    file = Path(P.file).expanduser()
-
-    data = load_data(file)
