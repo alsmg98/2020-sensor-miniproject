6b4424a548574dba917c0f3507f805ac148dba20
alsmg98 <70724837+alsmg98@users.noreply.github.com>
2020-09-16 20:44:30 -0400

    Update anomalies.py

--- anomalies.py
+++ anomalies.py
@@ -1,1 +1,47 @@
 """ Find anomalies """
+
+
+from pathlib import Path
+import argparse
+import pandas
+import matplotlib.pyplot as plt
+from scipy.stats import gamma
+import numpy as np
+
+from sp_iotsim.fileio import load_data
+
+
+i = (temp > 15) & (temp < 32)
+filtered_temp = temp[i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(description="load and analyse IoT JSON data")
+    p.add_argument("file", help="path to JSON data file")
+    p.add_argument("room", help="room to plot")
+    P = p.parse_args()
+
+    file = Path(P.file).expanduser()
+
+    data = load_data(file)
